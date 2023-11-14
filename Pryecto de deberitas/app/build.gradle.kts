7f68574a86411ecfcfe8255a97a43f929a0a0ef9
Roman <Roman@DESKTOP-RS415KS>
2023-11-13 18:50:47 -0600

    Repare lo que no servia

--- Pryecto de deberitas/app/build.gradle.kts
+++ Pryecto de deberitas/app/build.gradle.kts
@@ -47,11 +47,12 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.core:core-ktx:1.12.0")
