3fae410f86691c871d828d6b72eeed8f19e2d65f
Roman <Roman@DESKTOP-RS415KS>
2023-12-01 08:41:41 -0600

    Notificaciones funcionam mas o menos

--- Pryecto de deberitas/app/src/main/java/com/example/notas/ui/theme/Nota.kt
+++ Pryecto de deberitas/app/src/main/java/com/example/notas/ui/theme/Nota.kt
@@ -1,6 +1,15 @@
 package com.example.notas.ui.theme
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlarmManager
 import android.app.DatePickerDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -10,6 +19,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,6 +28,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -47,6 +58,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -59,6 +71,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -66,17 +81,24 @@ import com.example.notas.Navegacion.Screean
 import java.util.Calendar
 import java.util.Date
 import coil.compose.AsyncImage
+<<<<<<< HEAD
 import com.example.notas.AppViewModelProvider
 import com.example.notas.utils.NAvegationType
 //import com.example.notas.alarmas.AlarmSchedulerImpl
 //import com.example.notas.alarmas.AlarmasScreen
+=======
+import com.example.notas.R
+import com.example.notas.alarmas.AlarmSchedulerImpl
+import com.example.notas.alarmas.AlarmasScreen
+>>>>>>> aaaaf59 (Notificaciones funcionam mas o menos)
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ui.PlayerView
-import java.io.File
+import com.example.notas.ui.theme.notificacionProgramada.Companion.NOTIFICACION_ID
 
 
 class Nota : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -96,6 +118,7 @@ class Nota : ComponentActivity() {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.S)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopAppNotas(naveController: NavController,
@@ -136,41 +159,105 @@ fun TopAppNotas(naveController: NavController,
         }, mYear, mMonth, mDay
     )
 
+    val contestonotificaiones = LocalContext.current
+    val idCanal = "Tareas"
+    val idNotificacionTareas = 0
+
+    LaunchedEffect(Unit){
+        crearCanalNotificacion(idCanal,contestonotificaiones)
+
+    }
+
     CenterAlignedTopAppBar(
         title = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                IconButton(onClick = {naveController.navigate(Screean.NotasPrincipalScreean.route)}) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "Regresa al menu"
+                Row(Modifier.verticalScroll(rememberScrollState()),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(onClick = {naveController.navigate(Screean.NotasPrincipalScreean.route)}) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Regresa al menu"
+                        )
+                    }
+                    TextField(
+                        value = name,
+                        onValueChange = {name = it},
+                        label = { Text(text = "Titulo")},
+                        modifier = Modifier.fillMaxWidth(),
+                        trailingIcon = {
+                            IconButton(onClick ={
+                                                notificacionBasica(
+                                                    contestonotificaiones,
+                                                    idCanal,
+                                                    idNotificacionTareas,
+                                                    "Notificacion de tarea",
+                                                    "La fecha limite de la tarea finalizado"
+                                                )
+                                notificaionProgramada(contestonotificaiones)
+                                mDatePickerDialog.show()
+                            }, colors = IconButtonDefaults.iconButtonColors(Color(0,0,0, 0)) ){
+                                Icon(
+                                    imageVector = Icons.Outlined.DateRange,
+                                    contentDescription = "Fecha de tarea"
+                                )
+                            }
+                        }
                     )
                 }
-                TextField(
-                    value = name,
-                    onValueChange = {name = it},
-                    label = { Text(text = "Titulo")},
-                    modifier = Modifier.fillMaxWidth(),
-                    trailingIcon = {
-                        IconButton(onClick ={
-                        mDatePickerDialog.show()
-                        }, colors = IconButtonDefaults.iconButtonColors(Color(0,0,0, 0)) ){
-                            Icon(
-                                imageVector = Icons.Outlined.DateRange,
-                                contentDescription = "Fecha de tarea"
-                            )
-                        }
-                    }
-                )
 
-            }
         },
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
+    )
+}
+
+fun notificacionBasica(
+    contestonotificaiones: Context,
+    idCanal: String,
+    idNotificacionTareas: Int,
+    s: String,
+    s1: String,
+    prioriad: Int = NotificationCompat.PRIORITY_DEFAULT
+) {
+    val builder = NotificationCompat.Builder(contestonotificaiones,idCanal)
+        .setSmallIcon(androidx.media.R.drawable.notification_bg)
+        .setContentTitle(s)
+        .setContentText(s1)
+        .setPriority(prioriad)
+
+    with(NotificationManagerCompat.from(contestonotificaiones)){
+        if (ActivityCompat.checkSelfPermission(
+                contestonotificaiones,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notify(idNotificacionTareas,builder.build())
+    }
+}
+
+
+
+@SuppressLint("ScheduleExactAlarm")
+@RequiresApi(Build.VERSION_CODES.S)
+fun notificaionProgramada(mContext: Context) {
+    val intent = Intent(mContext,notificacionProgramada::class.java)
+    val penddingIntent = PendingIntent.getBroadcast(
+        mContext,
+        NOTIFICACION_ID,
+        intent,
+        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+    )
+    var alarmManager = mContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    alarmManager.setExact(
+        AlarmManager.RTC_WAKEUP,
+        Calendar.getInstance().timeInMillis+10000,penddingIntent
     )
 }
 
 
+@RequiresApi(Build.VERSION_CODES.S)
 @ExperimentalMaterial3Api
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -182,6 +269,7 @@ fun ScaffoldNotas(navController: NavController,
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     var showBottomSheet by remember { mutableStateOf(false) }
+    val mContext = LocalContext.current
 
 
     Scaffold(
@@ -388,8 +476,27 @@ fun VideoPlayer(videoUri: Uri, modifier: Modifier = Modifier.fillMaxWidth()) {
     }
 }
 
+<<<<<<< HEAD
 @Composable
 fun ContenidoNotas(viewModel: NotesViewModel = viewModel(factory = AppViewModelProvider.Factory), navController: NavController, navigationType:NAvegationType){
 
 
+=======
+// -----------------------------------------------------------------NOTIFICACION---------------------------------------------------------------
+
+fun crearCanalNotificacion(idCanal: String, contestonotificaiones: Context) {
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        val nombre = "CanalTareas"
+        val descripcion = "Canal de notificaciones de tareas"
+        val importancia = NotificationManager.IMPORTANCE_DEFAULT
+        val canal = NotificationChannel(idCanal,nombre,importancia)
+            .apply {
+                description = descripcion
+            }
+        val notificationManager:NotificationManager=
+            contestonotificaiones.getSystemService(Context.NOTIFICATION_SERVICE) as
+                    NotificationManager
+        notificationManager.createNotificationChannel(canal)
+    }
+>>>>>>> aaaaf59 (Notificaciones funcionam mas o menos)
 }
